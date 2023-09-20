5b8df590a34d4f9748ec6e813a46cc3e0a8e3290
JANGMINCHAE <145367176+JANGMINCHAE@users.noreply.github.com>
2023-09-20 11:10:12 +0900

    Add files via upload

--- PizzaTest.java
+++ PizzaTest.java
@@ -8,11 +8,14 @@ class Pizza{
 		size = 12;
 		type = "슈퍼슈프림";
 	}
-	
 	public Pizza(int size, String type) {
 		super();
 		this.size = size;
 		this.type = type;
+	}
+	@Override
+	public String toString() {
+		return "Pizza [size=" + size + ", type=" + type + "]";
 	}
 	
 }
@@ -22,9 +25,11 @@ public class PizzaTest {
 	public static void main(String[] args) {
 		Pizza obj1 = new Pizza();
 		System.out.println("("+obj1.type+", "+obj1.size+")");
-		
+		System.out.println(obj1);
 		Pizza obj2 = new Pizza(24, "포테이토");
 		System.out.println("("+obj2.type+", "+obj2.size+")");
+		System.out.println(obj2);
+		
 	}
 
 }
